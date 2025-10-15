f6146584bbc92036e8197c18bac500268b0e63f3
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2025-10-15 17:32:37 +0000

    Fixed a skill issue

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 // Host the public directory
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
